31f5b9211388c2536531febee39dcbcb8a3cfabc
h0yta <oscar@vettig.se>
2018-05-24 19:20:44 +0200

    Run airCooler

--- src/plugins/airCooler.js
+++ src/plugins/airCooler.js
@@ -6,13 +6,13 @@ const run = async (settings) => {
 
   if (temp > settings.threshold && settings.status === 'off') {
     log.debug(temp + ' is to hot, lets cool this place down');
-    //telldus.turnOn(settings.device);
-    //telldus.sendText(settings.notify, settings.device + " PÅ");
+    telldus.turnOn(settings.device);
+    telldus.sendText(settings.notify, settings.device + " PÅ");
     return 'on';
   } else if (temp < settings.threshold && settings.status === 'on') {
     log.debug(temp + ' is cool enough');
-    //telldus.turnOff(settings.device);
-    //telldus.sendText(settings.notify, settings.device + " AV");
+    telldus.turnOff(settings.device);
+    telldus.sendText(settings.notify, settings.device + " AV");
     return 'off';
   } else {
     log.debug('Temperature still ' + temp);
