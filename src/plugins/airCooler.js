bfa79cea383f256b12e8606e8ed07e42eb569b14
h0yta <oscar@vettig.se>
2018-05-26 07:33:14 +0200

    Bugfix in airCooler

--- src/plugins/airCooler.js
+++ src/plugins/airCooler.js
@@ -6,21 +6,27 @@ const run = async (settings) => {
   let temp = await telldus.temp(settings.sensorDevice);
 
   if (settings.workingHours) {
-    let now = dateFormat(new Date(), "HH:MM");
-    let start = dateFormat(new Date().setHours(
+    let now = new Date().getTime();
+    log.debug('0 -> ', now);
+
+    let start = new Date().setHours(
       settings.workingHours.start.split(':')[0],
-      settings.workingHours.start.split(':')[1]), 'HH:MM');
-    let stop = dateFormat(new Date().setHours(
+      settings.workingHours.start.split(':')[1]);
+    log.debug('1 -> ', start);
+
+    let stop = new Date().setHours(
       settings.workingHours.stop.split(':')[0],
-      settings.workingHours.stop.split(':')[1]), 'HH:MM');
+      settings.workingHours.stop.split(':')[1]);
+    log.debug('2 -> ', stop);
 
     if (now < start || now > stop) {
       log.debug('Outside working hours');
       if (settings.status === 'on') {
+        log.debug('Turn OFF');
         telldus.turnOff(settings.device);
         telldus.sendTexts(settings.notify, settings.device + " AV");
       }
-      return 'off';
+      return 'on';
     }
   }
 
