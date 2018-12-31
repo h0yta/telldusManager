3e88f377e2f0c655d672939f7ef9f54558f10ce2
hoyta <freakshow7>
2018-12-31 15:06:46 +0100

    Fix

--- src/plugins/doorBell.js
+++ src/plugins/doorBell.js
@@ -2,24 +2,18 @@ const telldus = require('../telldus');
 const log = require('loglevel');
 
 const run = async (settings) => {
-  console.log('run');
   let last = await telldus.lastAction(settings.device, settings.timeframe);
-  
+
   let now = parseInt(new Date().getTime() / 1000);
   let elapsedTime = parseInt(now - last.ts);
   let maxDelay = settings.delay + (settings.runEvery * 60);
 
-  console.log('elapsedTime', elapsedTime, 'delay', settings.delay, 'maxDelay', maxDelay);
-
-  if (last.state === 1 &&  elapsedTime > settings.delay && elapsedTime <= maxDelay) {
-    console.log('turn off', settings.device);
+  if (last.state === 1 && elapsedTime > settings.delay && elapsedTime <= maxDelay) {
     await telldus.turnOff(settings.device);
+    telldus.sendTexts(settings.notify, 'Dörrklocka återställd');
     settings.actionDevices.forEach(device => {
-      console.log('turn off', device);
       telldus.turnOff(device);
     });
-  } else {
-    console.log('state', last.state)
   }
 }
 
